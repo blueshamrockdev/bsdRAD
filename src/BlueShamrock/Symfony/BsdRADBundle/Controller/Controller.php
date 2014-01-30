a5acbc36490046b6b809a1f6c1c5caf7fe40a59b
Micah Breedlove <druid628@gmail.com>
2014-01-30 14:21:53 -0600

    Updates getRepository function to accept second arg of entity manager name

--- src/BlueShamrock/Symfony/BsdRADBundle/Controller/Controller.php
+++ src/BlueShamrock/Symfony/BsdRADBundle/Controller/Controller.php
@@ -6,46 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
 
 class Controller extends BaseController
 {
- 
-    /**
-     * Borrowed from KnpRAD Bundle
-     * @see https://github.com/KnpLabs/KnpRadBundle
-     *
-     * 
-     *
-     */
-    protected function findOr404($entity, $criterias = array())
-    {
-        $result = null;
-        $findMethod = is_scalar($criterias) ? 'find' : 'findOneBy';
-
-        if (is_object($entity) && $entity instanceof EntityRepository) {
-            $result = $entity->$findMethod($criterias);
-        } elseif (is_object($entity) && $this->getEntityManager()->contains($entity)) {
-            $result = $this->getEntityManager()->refresh($entity);
-        } elseif (is_string($entity)) {
-            $repository = $this->getRepository($entity);
-            $result = $repository->$findMethod($criterias);
-        }
-
-        if (null !== $result) {
-            return $result;
-        }
-
-        throw $this->createNotFoundException('Resource not found');
-    }
-
-    /**
-     * Get Repository 
-     *
-     * @param string $repo Bundle:Entity
-     *
-     * @return EntityRepository
-     */
-    public function getRepository($repo)
-    {
-        return $this->getDoctrine()->getRepository($repo);
-    }
 
     /**
      * Lazy function that gets a service or parameter by id.
@@ -56,12 +16,55 @@ class Controller extends BaseController
      */
     public function get($id)
     {
-        if($this->container->has($id)) {
+        if ($this->container->has($id)) {
             return $this->container->get($id);
-        } elseif($this->container->hasParameter($id)) {
+        } elseif ($this->container->hasParameter($id)) {
             return $this->container->getParameter($id);
-        } 
+        }
 
         return false;
+    }
+
+    /**
+     * Borrowed from KnpRAD Bundle
+     * @see https://github.com/KnpLabs/KnpRadBundle
+     *
+     * @param       $entity
+     * @param array $criterias
+     * @return null
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+    protected function findOr404($entity, $criterias = array())
+    {
+        $result     = null;
+        $findMethod = is_scalar($criterias) ? 'find' : 'findOneBy';
+
+        if (is_object($entity) && $entity instanceof EntityRepository) {
+            $result = $entity->$findMethod($criterias);
+        } elseif (is_object($entity) && $this->getEntityManager()->contains($entity)) {
+            $result = $this->getEntityManager()->refresh($entity);
+        } elseif (is_string($entity)) {
+            $repository = $this->getRepository($entity);
+            $result     = $repository->$findMethod($criterias);
+        }
+
+        if (null !== $result) {
+            return $result;
+        }
+
+        throw $this->createNotFoundException('Resource not found');
+    }
+
+    /**
+     * Get Repository
+     *
+     * @param string      $repo    Bundle:Entity
+     * @param string|null $manager entity manager name
+     *
+     * @return EntityRepository
+     */
+    public function getRepository($repo, $manager = null)
+    {
+        return $this->getDoctrine()->getManager($manager)->getRepository($repo);
     }
 }
